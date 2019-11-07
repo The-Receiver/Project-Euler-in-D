ebbbed1c73bbabb1e5b75a2dbddb59007c93a05f
The-Receiver <47209193+The-Receiver@users.noreply.github.com>
2019-11-07 20:47:31 +0630

    Update euler99.d

--- euler99.d
+++ euler99.d
@@ -1,4 +1,11 @@
-import std.stdio,std.range,
+//compile with dmd -J. euler99.d
+/* Goals for later reimplementation 
+
+Clean up line count (Was going to do it with a foreach loop but
+that didn't work for some reason)
+
+*/
+import std.stdio, std.range,
 std.conv, std.string, std.math;
 void main(){
 	auto numbers = File("base_exp.bin", "r");
@@ -8,11 +15,11 @@ void main(){
 	int lineno = 0;	
 	foreach(line; lines){
 		string[] expair = cast(string[]) split(line, ",");
-
 		real base = log(to!int(expair[0]));
 		int exp = to!int(expair[1]);
+		
 		real v = base * exp;
-		if (max < v){ max = v; lineno = linecnt;}
+		if (max < v){max = v; lineno = linecnt;}
 		
 	linecnt++;
 	}	
